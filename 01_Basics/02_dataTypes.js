e7642b6839d222a8d9aa3c0b065141382054620f
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-01-20 19:39:44 +0530

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //use all code in newer standards, need not to write as it is used by default generally 
+// we are using node so alert wont work here like in browser
+
+let name = "John";
+let age = 25;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
+
+
+// int is 2^53 
+// there is also bigint type which is used for large numbers which is used by appending n at the end of number.
+// string  write in ""
+// boolean true or false
+// null is a standalone value which represents the absence of an object value, but it is defined 
+// undefined is a type itself, it means a variable has been declared but has not yet been assigned a value.
+// symbol is used for unique identifiers for objects
+
+// ^^ these are premitive datatypes
+
+// object 
+
+console.log(typeof undefined); // is undefined
+//but 
+console.log(typeof null); // is object and not a type (interview question)
+
