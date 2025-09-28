6813c3c8c01e23cfe8b9046f4ca693265b3377b4
Prateek7071 <tiwariprateek123@gmail.com>
2025-09-29 02:14:25 +0530

    private #

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -51,4 +51,39 @@ console.log(boy.email);
 console.log(boy.password);
 console.log(boy._email); // it will show the actual value of the property 
 
+//The new way for private properties is to use # before the property name but it is not supported in all browsers yet
 
+class NewUser {
+    #email
+    #password
+    constructor(email, password, age){
+        this.#email = email;
+        this.#password = password;
+        this.age = age;
+    }
+
+    get email(){
+        return this.#email.toUpperCase();
+    }
+    set email(value){
+        this.#email = value;
+    }
+
+    get password(){
+        return `${this.#password}hisdhishid`;
+    }
+
+    set password(value){
+        if(value.length < 6){
+            throw new Error("Password must be at least 6 characters long"); //it will throw error if password is less than 6 characters
+        }
+        this.#password = value; // it will not allow to access the property directly
+    }
+
+}
+
+const newBoy = new NewUser("example@mail.com", "hdbskdjgfs" , 25);
+console.log(newBoy.email); 
+console.log(newBoy.password); 
+console.log(newBoy.#email); // it will throw error as #email is a private property
+console.log(newBoy.#password); // it will throw error as #password is a private property
