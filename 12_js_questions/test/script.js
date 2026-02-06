006f779bcf7ef57e4bab424ba4367c8cd6e333a4
Prateek7071 <tiwariprateek123@gmail.com>
2026-02-06 22:48:17 +0530

    2

--- 12_js_questions/test/script.js
+++ 12_js_questions/test/script.js
@@ -1,6 +1,30 @@
-//your JS code here. If required
-document.querySelector('input[type="button"]').addEventListener('click',(e)=>{
-	let scolor = document.getElementById('colorSelect')
-	if(scolor.selectedIndex !== -1)
-	scolor.remove(scolor.selectedIndex)
-})
+//your JS code here. If required.
+const abtn= document.getElementById('applause')
+const bbtn= document.getElementById('boo')
+const gbtn= document.getElementById('gasp')
+const tbtn= document.getElementById('tada')
+const vbtn= document.getElementById('victory')
+const wbtn= document.getElementById('wrong')
+const sbtn= document.getElementById('stop')
+
+const audio = [
+  applause,
+  boo,
+  gasp,
+  tada,
+  victory,
+  wrong
+]
+let url
+
+function playAudio(track){
+  url = `./${track}`
+  const audioBit = new Audio(url)
+  audioBit.play()
+}
+
+function pauseAudio(){
+  const audioBit = new Audio(url)
+  audioBit.pause()
+}
+
