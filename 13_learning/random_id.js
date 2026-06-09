e967bd8a94fddca420df69e9a199b47a65c3e882
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-06-09 23:49:43 +0530

    Update random_id.js

--- 13_learning/random_id.js
+++ 13_learning/random_id.js
@@ -1,7 +1,6 @@
   let numList =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 function makeid(l) {
-  // write your code here
   let res = "";
   for (let i = 0; i < l; i++) {
     res += numList.charAt(Math.floor(Math.random() * 62));
